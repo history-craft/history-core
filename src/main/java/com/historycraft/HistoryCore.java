08de300e5e49ebac12a7ba993e154fa58a264956
squall-tech <squall-tech@outlook.com>
2019-05-24 23:27:26 +0100

    aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa

--- src/main/java/com/historycraft/HistoryCore.java
+++ src/main/java/com/historycraft/HistoryCore.java
@@ -18,11 +18,11 @@ public class HistoryCore {
 
     public static final String MODID = "historycore";
     public static final String NAME = "History Core";
-    public static final String VERSION = "1.1.6";
+    public static final String VERSION = "1.1.7";
 
     public static Logger logger;
 
-    @SidedProxy(modId = HistoryCore.MODID, clientSide = "com.historycraft.ClientProxy", serverSide = "gcom.historycraft.CommonProxy")
+    @SidedProxy(modId = HistoryCore.MODID, clientSide = "com.historycraft.ClientProxy", serverSide = "com.historycraft.CommonProxy")
     public static CommonProxy proxy;
 
     @Mod.EventHandler
